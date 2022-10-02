f0718016c00327e7f69769bb44b1067c1246946d
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2022-10-02 18:08:03 -0500

    API Productos

--- API/API_Manager.js
+++ API/API_Manager.js
@@ -20,17 +20,6 @@ class Productos {
         }
     }
 
-    setProduct(new_data){
-        try{
-            const data = this.getAll();
-            data.push(new_data);
-            fs.writeFileSync(this.filename, JSON.stringify(data, null, 2))
-            return data;
-        }catch(e){
-            this.log(e);
-        }
-    }
-
     getById(id){
         try{
             const data = this.getAll();
@@ -39,6 +28,87 @@ class Productos {
         }catch(e){
             this.log(e);
         }
+    }
+
+    setProduct(new_data){
+        try{
+            const data = this.getAll();
+            const timestamp = this.setTimestamp(new Date());//verificar su funcionamiento
+            let id = 0, code = this.generateCode();
+            data.map((elemento) => {
+                if(id < Math.max(elemento.id)){
+                    id = Math.max(elemento.id);
+                }
+            })
+            new_data['code']=code;
+            new_data['id']=id+1;
+            new_data['timestamp']=timestamp;
+            data.push(new_data);
+            fs.writeFileSync(this.filename, JSON.stringify(data, null, 2))
+            return true;
+        }catch(e){
+            this.log(e);
+            return false;
+        }
+    }
+
+    updateProduct(new_data, old_id){
+        try{
+            const data = this.getAll();
+            let id = 0;
+            data.map((elemento) => {
+                if(id < Math.max(elemento.id)){
+                    id = Math.max(elemento.id);
+                }
+            })
+            new_data['id'] = id+1;
+            data[data.findIndex((elemento) => elemento.id == old_id)] = new_data;
+            fs.writeFileSync(this.filename, JSON.stringify(data, null, 2));
+            return true;
+        }catch(e){
+            this.log(e);
+            return false;
+        }
+    }
+
+    deleteByID(id) {
+        try{
+            const data = this.getAll();
+            if(data.findIndex((elemento) => elemento.id == id) == -1){
+                throw new Error;
+            }
+            const new_data = data.filter((elemento) => elemento.id != id);
+            fs.writeFileSync(this.filename, JSON.stringify(new_data, null, 2));
+            return true;
+        }catch(e){
+            if(e!='Error'){this.log(e)};
+            return false;
+        }
+    }
+
+    validateProduct(producto){
+        try{
+            if(producto.id){throw new Error}
+            if(!producto.tittle){throw new Error}
+            if(!producto.description){throw new Error}
+            if(!producto.thumbnail){throw new Error}
+            if(!producto.price){throw new Error}
+            if(!producto.stock){throw new Error}
+            return true;
+        }catch(e){
+            if(e!='Error'){this.log(e)};
+            return false;
+        }
+    }
+
+    setTimestamp(date) {
+        return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
+    }
+
+    generateCode(){
+        return Math.floor((Math.random() * (99 - 10 + 1)) + 10)+
+            '-'+Math.floor((Math.random() * (999 - 100 + 1)) + 100)+
+            '-'+Math.floor((Math.random() * (9999 - 1000 + 1)) + 1000);
     }
 
     log(e){
--- Routers/Router_API.js
+++ Routers/Router_API.js
@@ -5,37 +5,54 @@ const BaseDir = path.join(__dirname, '../');
 const {Productos} = require('../API/API_Manager');
 const BD_Productos = new Productos();
 
+function validateAdmin(req, res, next) {
+    if(String((req.query.admin)).toLowerCase() == "true"){
+        next();
+    }else{
+        res.json({
+            error: -1,
+            descripción: {ruta: req.url,
+                método: req.method,
+            },
+            estado: 'No autorizado'
+        });
+    }
+}
+
 API.get('/api/productos/', (req, res) => {
     res.json(BD_Productos.getAll());
 });
 
-API.get('/api/productos/:id?', (req, res) => {
-    if((req.url).indexOf('?')!=-1){
-        let url = ((req.url).slice((req.url).indexOf('?')+1)).replace('=','":');
-        url = JSON.parse('{'+'"'+url+'}')
-        console.log(url);
-        if(url.admin){
-            res.json(BD_Productos.getById(parseInt(req.params.id)));
-        }
-    }
-    res.json({
-        error: -1,
-        descripción: {ruta: req.url,
-            método: req.method,
-        },
-        estado: 'No autorizado'
-    });
+API.get('/api/productos/:id', validateAdmin, (req, res) => {
+    res.json(BD_Productos.getById(parseInt(req.params.id)));
 });
 
-API.post('/api/productos', (req, res) => {
-    if((req.url).indexOf('?')!=-1){
-        let url = ((req.url).slice((req.url).indexOf('?')+1)).replace('=','":');
-        console.log(url);
-        res.json({status: 'OK'});
+API.post('/api/productos', validateAdmin, (req, res) => {
+    if(BD_Productos.validateProduct(req.body)){
+        BD_Productos.setProduct(req.body)
+            ? res.json({status: 'OK'})
+            : res.json({status: 'ERROR - an error was encountered while inserting the product'})
     }else{
-        res.json({status: 'ERR'})
+        res.json({status: 'ERROR - invalid JSON structure'})
     }
-    
-})
+});
+
+API.put('/api/productos/:id', validateAdmin, (req, res) => {
+    if(BD_Productos.validateProduct(req.body)){
+        BD_Productos.updateProduct(req.body, parseInt(req.params.id))
+            ? res.json({status: 'OK'})
+            : res.json({status: 'ERROR - an error was encountered while inserting the product'})
+    }else{
+        res.json({status: 'ERROR - invalid JSON structure'})
+    }
+});
+
+API.delete('/api/productos/:id', validateAdmin, (req, res) => {
+    if(BD_Productos.deleteByID(parseInt(req.params.id))){
+        res.json({status: "OK"});
+    }else{
+        res.json({status: "ERROR - ID not exists"});
+    }
+});
 
 module.exports = API;
