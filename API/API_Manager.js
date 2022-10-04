0ea1e9c2c31376b82ba5e7e80fd57f8c13dd555c
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2022-10-03 19:44:37 -0500

    API Carrito

--- API/API_Manager.js
+++ API/API_Manager.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 class Productos {
     
     constructor(){
-        this.filename = __dirname+'\\Products.json';
+        this.filename = 'Persistent_Files\\Products.json';
     }
 
     getAll(){
@@ -24,9 +24,14 @@ class Productos {
         try{
             const data = this.getAll();
             const producto = data.find((elemento) => elemento.id === id);
-            return producto;
+            if(typeof producto != 'undefined'){
+                return producto;
+            }else{
+                return false;
+            }
         }catch(e){
             this.log(e);
+            return false;
         }
     }
 
@@ -40,6 +45,11 @@ class Productos {
                     id = Math.max(elemento.id);
                 }
             })
+            data.map((elemento) => {
+                if(elemento.id == code){
+                    throw new Error
+                }
+            });
             new_data['code']=code;
             new_data['id']=id+1;
             new_data['timestamp']=timestamp;
@@ -119,4 +129,145 @@ class Productos {
     }
 }
 
-module.exports = {Productos};
+class Carrito{
+
+    constructor(){
+        this.filename = 'Persistent_Files\\Carrito.json';
+        this.Productos = new Productos();
+    }
+
+    getAll(){
+        try{
+            let data = fs.readFileSync(this.filename, 'utf8');
+            if(data == ''){
+                fs.writeFileSync(this.filename, "[]")
+                return this.getAll();
+            }else{
+                return JSON.parse(data);
+            } 
+        }catch(e){
+            this.log(e);
+        }
+    }
+
+    getById(id){
+        try{
+            const data = this.getAll();
+            const producto = data.find((elemento) => elemento.id === id);
+            if(typeof producto != 'undefined'){
+                return producto;
+            }else{
+                return false;
+            }
+        }catch(e){
+            this.log(e);
+            return false;
+        }
+    }
+
+    createCarrito(){
+        try{
+            const data = this.getAll();
+            const timestamp = this.setTimestamp(new Date());//verificar su funcionamiento
+            let id = 0, new_data = {};
+            data.map((elemento) => {
+                if(id < Math.max(elemento.id)){
+                    id = Math.max(elemento.id);
+                }
+            })
+            new_data['id']=id+1;
+            new_data['timestamp']=timestamp;
+            new_data['productos']=[];
+            data.push(new_data);
+            fs.writeFileSync(this.filename, JSON.stringify(data, null, 2))
+            return id;
+        }catch(e){
+            this.log(e);
+            return false;
+        }
+    }
+
+    setProduct(id_carrito, id_producto){
+        try{
+            const data = this.getAll();
+            const index = data.findIndex((elemento) => elemento.id == id_carrito);
+            if(index == -1){throw new Error('No se encontró el ID_carrito')}
+            const product = this.Productos.getById(id_producto);
+            if(typeof product != 'boolean'){
+                data[index].productos.push(product)
+            }else{
+                throw new Error('El ID_producto no existe');
+            }
+            fs.writeFileSync(this.filename, JSON.stringify(data, null, 2))
+            return [true];
+        }catch(e){
+            this.log(e);
+            return [false, e];
+        }
+    }
+
+    deleteProduct(id_carrito, id_producto){
+        try{
+            const data = this.getAll();
+            const index = data.findIndex((elemento) => elemento.id == id_carrito);
+            if(index == -1){throw new Error('No se encontró el ID_carrito')};
+            const index_product = data[index].productos.findIndex((elemento) => elemento.id == id_producto);
+            if(index_product == -1){throw new Error('No se encontró el ID_producto')};
+            data[index].productos = data[index].productos.filter(producto => producto.id != id_producto);//FALTA APLICAR LAS VALIDACIONES CORRESPONDIENTES Y RETORNAR ARRAY
+            fs.writeFileSync(this.filename, JSON.stringify(data, null, 2));
+            return [true];
+        }catch(e){
+            this.log(e);
+            return [false, e];
+        }
+    }
+
+    deleteByID(id) {
+        try{
+            const data = this.getAll();
+            if(data.findIndex((elemento) => elemento.id == id) == -1){
+                throw new Error('No se encontró el ID');
+            }
+            const new_data = data.filter((elemento) => elemento.id != id);
+            fs.writeFileSync(this.filename, JSON.stringify(new_data, null, 2));
+            return true;
+        }catch(e){
+            if(e!='Error'){this.log(e)};
+            return false;
+        }
+    }
+
+    validateProduct(producto){
+        try{
+            if(producto.id){throw new Error}
+            if(!producto.tittle){throw new Error}
+            if(!producto.description){throw new Error}
+            if(!producto.thumbnail){throw new Error}
+            if(!producto.price){throw new Error}
+            if(!producto.stock){throw new Error}
+            return true;
+        }catch(e){
+            if(e!='Error'){this.log(e)};
+            return false;
+        }
+    }
+
+    setTimestamp(date) {
+        return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
+    }
+
+    generateCode(){
+        return Math.floor((Math.random() * (99 - 10 + 1)) + 10)+
+            '-'+Math.floor((Math.random() * (999 - 100 + 1)) + 100)+
+            '-'+Math.floor((Math.random() * (9999 - 1000 + 1)) + 1000);
+    }
+
+    log(e){
+        let date = new Date();
+        let current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+        fs.appendFileSync("log.txt", "\n\n"+current_time.toString()+"\n");
+        fs.appendFileSync("log.txt", e.toString());
+    }
+}
+
+module.exports = {Productos, Carrito};
--- Routers/Router_API.js
+++ Routers/Router_API.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const API = express.Router();
 const BaseDir = path.join(__dirname, '../');
-const {Productos} = require('../API/API_Manager');
+const {Productos, Carrito} = require('../API/API_Manager');
 const BD_Productos = new Productos();
+const BD_Carrito = new Carrito();
+let isLoggedIn = false;
 
 function validateAdmin(req, res, next) {
-    if(String((req.query.admin)).toLowerCase() == "true"){
+    if(String((req.query.admin)).toLowerCase() == "true" || isLoggedIn){
+        isLoggedIn = true;
         next();
     }else{
         res.json({
@@ -19,12 +22,17 @@ function validateAdmin(req, res, next) {
     }
 }
 
+/* API PRODUCTOS */
+
 API.get('/api/productos/', (req, res) => {
     res.json(BD_Productos.getAll());
 });
 
 API.get('/api/productos/:id', validateAdmin, (req, res) => {
-    res.json(BD_Productos.getById(parseInt(req.params.id)));
+    const producto = BD_Productos.getById(parseInt(req.params.id));
+    typeof producto != 'boolean'
+        ? res.json(producto)
+        : res.json({status: 'ERROR - ID Product not exists'})
 });
 
 API.post('/api/productos', validateAdmin, (req, res) => {
@@ -33,7 +41,7 @@ API.post('/api/productos', validateAdmin, (req, res) => {
             ? res.json({status: 'OK'})
             : res.json({status: 'ERROR - an error was encountered while inserting the product'})
     }else{
-        res.json({status: 'ERROR - invalid JSON structure'})
+        res.json({status: 'ERROR - invalid JSON structure'});
     }
 });
 
@@ -52,6 +60,58 @@ API.delete('/api/productos/:id', validateAdmin, (req, res) => {
         res.json({status: "OK"});
     }else{
         res.json({status: "ERROR - ID not exists"});
+    }
+});
+
+/* API CARRITO */
+
+API.get('/api/carrito/:id/productos', (req, res) => {
+    const carrito = BD_Carrito.getById(parseInt(req.params.id));
+    if(typeof carrito != 'boolean'){
+        carrito.productos.length != 0
+            ? res.json(carrito.productos)
+            : res.json({productos: 'EMPTY'});
+    }else{
+        res.json({status: 'ERROR - ID carrito not exists'})
+    }
+});
+
+API.post('/api/carrito/', (req, res) => {
+    const new_carrito = BD_Carrito.createCarrito();
+    typeof id != 'boolean'
+        ? res.json({new_carrito_id: new_carrito})
+        : res.json({status: 'ERROR - while creating carrito'})
+});
+
+API.post('/api/carrito/:id1/productos/:id2', (req, res) => {
+    try{
+        const status = BD_Carrito.setProduct(parseInt(req.params.id1), parseInt(req.params.id2));
+        if(status.length == 1) {
+            res.json({status: 'OK'});
+        }else{
+            res.json({status: 'ERROR - '+status[1].message});
+        }
+    }catch{
+        res.json({status: 'ERROR - an error was encountered while processing the request'});
+    }
+});
+
+API.delete('/api/carrito/:id', (req, res) => {
+    BD_Carrito.deleteByID(parseInt(req.params.id))
+        ? res.json({status: 'OK'})
+        : res.json({status: 'ERROR'});
+});
+
+API.delete('/api/carrito/:id/productos/:id_prod', (req, res) => {
+    try{
+        const status = BD_Carrito.deleteProduct(parseInt(req.params.id), parseInt(req.params.id_prod))
+        if(status.length == 1) {
+            res.json({status: 'OK'});;
+        }else{
+            res.json({status: 'ERROR - '+status[1].message});
+        }
+    }catch{
+        res.json({status: 'ERROR - an error was encountered while processing the request'});
     }
 });
 
