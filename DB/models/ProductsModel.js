c92861e09e228906877f035fdfd7f58c726b8893
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2023-03-06 17:48:44 -0500

    Replace code for category

--- DB/models/ProductsModel.js
+++ DB/models/ProductsModel.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     title: { type: String, required: true },
     brand: { type: String, required: true },
     timestamp: { type: Date, default: Date.now },
-    code: { type: String, required: true },
+    category: { type: String, required: true },
     thumbnail: { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, required: true },
